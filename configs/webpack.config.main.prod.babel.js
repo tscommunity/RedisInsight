a2788d843b61ec694a23fa65305e05e9bb54adcc
vlad-dargel <u.darhel@aisnovations.com>
2023-06-14 10:31:59 +0200

    add telemetry

--- configs/webpack.config.main.prod.babel.js
+++ configs/webpack.config.main.prod.babel.js
@@ -70,7 +70,7 @@ export default merge(baseConfig, {
       BUILD_TYPE: 'ELECTRON',
       APP_VERSION: version,
       AWS_BUCKET_NAME: 'AWS_BUCKET_NAME' in process.env ? process.env.AWS_BUCKET_NAME : '',
-      SEGMENT_WRITE_KEY: 'SEGMENT_WRITE_KEY' in process.env ? process.env.SEGMENT_WRITE_KEY : 'SOURCE_WRITE_KEY',
+      SEGMENT_WRITE_KEY: 'MWGOG146oPdLSWO5mZy3eM1NzcC3alRF',
       CONNECTIONS_TIMEOUT_DEFAULT: 'CONNECTIONS_TIMEOUT_DEFAULT' in process.env
         ? process.env.CONNECTIONS_TIMEOUT_DEFAULT
         : toString(30 * 1000), // 30 sec
--- configs/webpack.config.renderer.dev.babel.js
+++ configs/webpack.config.renderer.dev.babel.js
@@ -214,8 +214,7 @@ export default merge(baseConfig, {
       PIPELINE_COUNT_DEFAULT: '5',
       BUILD_TYPE: 'ELECTRON',
       APP_VERSION: version,
-      SEGMENT_WRITE_KEY:
-        'SEGMENT_WRITE_KEY' in process.env ? process.env.SEGMENT_WRITE_KEY : 'SOURCE_WRITE_KEY',
+      SEGMENT_WRITE_KEY: 'MWGOG146oPdLSWO5mZy3eM1NzcC3alRF',
       CONNECTIONS_TIMEOUT_DEFAULT: 'CONNECTIONS_TIMEOUT_DEFAULT' in process.env
         ? process.env.CONNECTIONS_TIMEOUT_DEFAULT
         : toString(30 * 1000), // 30 sec
