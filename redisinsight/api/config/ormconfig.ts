a57a6f32e191a718d2a358a7716b16d97a3312e5
Artem <artem.horuzhenko@softeq.com>
2023-08-24 11:30:11 +0300

    t

--- redisinsight/api/config/ormconfig.ts
+++ redisinsight/api/config/ormconfig.ts
@@ -18,9 +18,9 @@ import { CustomTutorialEntity } from 'src/modules/custom-tutorial/entities/custo
 import { FeatureEntity } from 'src/modules/feature/entities/feature.entity';
 import { FeaturesConfigEntity } from 'src/modules/feature/entities/features-config.entity';
 import { CloudDatabaseDetailsEntity } from 'src/modules/cloud/database/entities/cloud-database-details.entity';
+import { CloudCapiKeyEntity } from 'src/modules/cloud/capi-key/entity/cloud-capi-key.entity';
 import migrations from '../migration';
 import * as config from '../src/utils/config';
-import { CloudCapiKeyEntity } from 'src/modules/cloud/capi-key/entity/cloud-capi-key.entity';
 
 const dbConfig = config.get('db');
 
